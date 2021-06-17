f1745161fdefccacd87308e92da6be4e768a8ad1
rnoboa <rnoboa22@gmail.com>
2021-06-16 21:03:10 -0500

    feat: edit complete.

--- customers/urls.py
+++ customers/urls.py
@@ -1,10 +1,12 @@
 """customers/urls.py"""
 from django.urls import path
 
-from customers.views import customers_list, customers_create
+from customers.views import customers_list, customers_create, customer_detail, customers_edit
 
 app_name = "customers"
 urlpatterns = [
     path('', customers_list, name="customers_list"),
-    path('create/', customers_create, name="customers_create")
+    path('create/', customers_create, name="customers_create"),
+    path('detail/<int:identifier>/', customer_detail, name="customers_detail"),
+    path('edit/<int:identifier>/', customers_edit, name="customers_edit")
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib import messages
@@ -8,14 +10,19 @@ from customers.models import CustomerModel
 
 
 # LISTAR
-
-
 def customers_list(request):
     customers = CustomerModel.objects.filter(status=True).order_by("full_name")
     return render(request, "customers/list.html", {"customers": customers})
 
 
 # OBTENER
+def customer_detail(request, identifier: int):
+    try:
+        customer = CustomerModel.objects.get(id=identifier)
+    except CustomerModel.DoesNotExist:
+        customer = None
+    return render(request, "customers/detail.html", {"customer": customer})
+
 
 # CREAR
 def customers_create(request):
@@ -24,16 +31,41 @@ def customers_create(request):
         form = BasicCustomerForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data  # -> dict {"full_name": <>, "dni": <>}
-            customer = CustomerModel()
-            customer.full_name = data.get('full_name')
-            customer.dni = data.get('dni')
-            customer.save()
+            # customer = CustomerModel()
+            # customer.full_name = data.get('full_name')
+            # customer.dni = data.get('dni')
+            # customer.save()
+            CustomerModel.objects.create(full_name=data.get("full_name"), dni=data.get("dni"))  # ORM
             messages.success(request, "Cliente registrado exitosamente.")
             return redirect(reverse_lazy("customers:customers_list"))
         else:
             messages.error(request, "Error al crear un cliente")
     return render(request, 'customers/create.html', {"form": form})
 
+
 # ACTUALIZAR
+def customers_edit(request, identifier: int):
+    # 1. Obtener el registro a editar.
+    try:
+        customer = CustomerModel.objects.get(id=identifier)
+    except CustomerModel.DoesNotExist:
+        customer = None
+
+    form = BasicCustomerForm()
+    if customer is not None:
+        form = BasicCustomerForm(initial={"full_name": customer.full_name, "dni": customer.dni})
+        if request.method == "POST":
+            form = BasicCustomerForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                customer.full_name = data.get("full_name")
+                customer.dni = data.get("dni")
+                customer.modified_at = datetime.now()
+                customer.save()
+                messages.success(request, "Cliente modificado exitosamente.")
+                return redirect(reverse_lazy("customers:customers_list"))
+            else:
+                messages.error(request, "Error al editar un cliente")
+    return render(request, "customers/update.html", {"form": form})
 
 # BORRAR
